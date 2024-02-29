e86f43b168939ec6a6ca36ce57f5279ebfdd2703
Sizzlesina <www.sins604@gmail.com>
2024-02-29 11:19:09 +0330

    Initial commit

---
+++ 1-typescript-basics/first-app.ts
@@ -0,0 +1,47 @@
+/** @format */
+
+let userName = "Max";
+// userName = 34; // this cant be done because typescript care about the types and this variable is a string typed variable
+
+// !HINT: But in javascript we can do this
+// string type
+let Name: string = "dad";
+// number type
+let userAge: number = 20;
+// boolean type
+let isValid: boolean = true;
+
+// @ Sometime we need a variable to be more than one type for example somewhere it should be string and somewhere it should be number
+
+let userId: string | number = "abc";
+console.log(userId);
+userId = 123;
+console.log(userId);
+
+// let user;
+// let user : object;
+let user: {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+  id: string | number;
+};
+user = {
+  name: "Max",
+  age: 12,
+  isAdmin: true,
+  id: "abc",
+};
+
+// let hobbies: Array<string>;
+let hobbies: string[]; // number[],boolean[]
+// {name : string,age: number}
+
+hobbies = ["Sports", "Cooking", "Reading"];
+
+const API_KEY = "abc";
+
+function add(a: number, b: number): void {
+  const result = a + b;
+  console.log(result);
+}
---
+++ notes/FF1.notes.js
@@ -0,0 +1,70 @@
+/** @format */
+
+{/* prettier-ignore */}
+
+/*
+@ How to install typescript?: 
+++ - For installing the typescript we need a compiler which we can install it globally on our system this commands:
+1 - npm install -g typescript
+2 - npx tsc
+! Source Link : https://www.typescriptlang.org/ and download the typescript
+
+@ Compiling the typescript file:
+- In the typescript project we can compile the typescript file to a javascript file by using this command:
+@ npx tsc .\fileName
+
+@ Difference between typescript code and javascript code:
+++ javascript code:
+- Runs in the browser
+- Has no extra-type-safety features built-in -> Not ideal for development
+
+function add(a , b){
+  return a + b;
+}
+
+++ typescript code:
+- Does not run in the browser
+- Great for development as it adds extra type-safety & catches potential errors
+
+function add (a:number , b : number){
+  return a + b;
+}
+
+@ Types in typescript:
+++ number
+++ string
+++ any 
+++ boolean
+++ object
+++ {} :
+- in this way of setting the type we can set the name ana type of each property 
+! Example:
+let user = {
+  name : string;
+  age : number;
+  isAdmin : boolean;
+  id : number | string;
+}
+user = {
+  name : "Sina",
+  age : 20,
+  isAdmin : true,
+  id : 123,
+}
+++ Array<type>:
+- In this type we have a array and in the <> we write the type of the values of the array
+++ type[]:
+- Also we can write the Array<type> like this (These two are simillar)
+
+++ undefined :
+- We can use this type for the functions that doesnt return anything
+++ void:
+- Also we can use this type for the functions that doesnt return anything
+
+
+@ How to write functions in TS:
+function functionName(parameter1: type, paremeter2: type): return type of the function {
+  commands
+}
+
+*/
