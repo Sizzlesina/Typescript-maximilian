b34ef9c6ce3e516330f55ec43111f1697aeb715a
Sizzlesina <www.sins604@gmail.com>
2024-02-29 12:43:32 +0330

    Type guards

--- 1-typescript-basics/first-app.ts
+++ 1-typescript-basics/first-app.ts
@@ -110,3 +110,16 @@ admin = {
   permission: ["login"],
   username: "sina",
 };
+
+type Role = "admin" | "user" | "editor";
+let role: Role;
+role = "admin";
+role = "user";
+role = "editor";
+//! role = "abc";
+
+function performAction(action: string | number, role: Role) {
+  if (role === "admin" && typeof action === "string") {
+    // ...
+  }
+}
