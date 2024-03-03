6199c1e81d544a2ce192874cb865201559bddf79
Sizzlesina <www.sins604@gmail.com>
2024-03-03 13:46:23 +0330

    Handling event types

---
+++ 2-react-ts-basics/src/components/NewGoal.tsx
@@ -0,0 +1,27 @@
+/** @format */
+
+import { type FormEvent } from "react";
+
+function NewGoal() {
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label htmlFor='goal'>Your goal</label>
+        <input type='text' id='goal' />
+      </p>
+      <p>
+        <label htmlFor='summary'>Short Summary</label>
+        <input type='text' id='summary' />
+      </p>
+      <p>
+        <button>Add Goal</button>
+      </p>
+    </form>
+  );
+}
+
+export default NewGoal;
