f0f05265e8263976ffcb69a79127d26fd135ca49
Sizzlesina <www.sins604@gmail.com>
2024-03-04 20:06:50 +0330

    53 - Working with Type Predicates Facing TypeScript Limitations

--- 3-advanced-components/src/App.tsx
+++ 3-advanced-components/src/App.tsx
@@ -6,8 +6,6 @@ import Input from "./components/Input";
 function App() {
   return (
     <main>
-      <Input label='Your name' id='name' type='text' value={"Sina"} disabled />
-      <Input label='Your age' id='age' type='number' />
       <p>
         <Button el='button'>A Button</Button>
       </p>
--- 3-advanced-components/src/components/Button.tsx
+++ 3-advanced-components/src/components/Button.tsx
@@ -2,16 +2,21 @@
 
 import { type ComponentPropsWithoutRef } from "react";
 
-type ButtonProps = {
-  el: "button";
-} & ComponentPropsWithoutRef<"button">;
-type AnchorProps = {
-  el: "anchor";
-} & ComponentPropsWithoutRef<"a">;
+type ButtonProps = ComponentPropsWithoutRef<"button"> & {
+  href?: never;
+};
+
+type AnchorProps = ComponentPropsWithoutRef<"a"> & {
+  href?: string;
+};
+
+function isAnchorProps(props: ButtonProps | AnchorProps): props is AnchorProps {
+  return "href" in props;
+}
 function Button(props: ButtonProps | AnchorProps) {
   // const { el, ...otherProps } = props;
 
-  if (props.el === "anchor") {
+  if (isAnchorProps(props)) {
     return <a className='button' {...props}></a>;
   }
 
