23c7de3ff135e929674cb045e6398b9c387d881a
Sizzlesina <www.sins604@gmail.com>
2024-03-06 12:43:26 +0330

    Advanced State Management project starter files added

---
+++ 4-advanced-state-management/src/App.tsx
@@ -0,0 +1,18 @@
+/** @format */
+import AddTimer from './components/AddTimer.tsx';
+import Header from './components/Header.tsx';
+import Timers from './components/Timers.tsx';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ 4-advanced-state-management/src/components/Timer.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import Container from "./UI/Container.tsx";
+
+export default function Timer() {
+  return (
+    <Container as='article'>
+      <h2>TODO: TIMER NAME</h2>
+    </Container>
+  );
+}
