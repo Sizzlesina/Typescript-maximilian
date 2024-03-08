b49083a11e4bc6179f2c6df99bbdf2871e6afd41
Sizzlesina <www.sins604@gmail.com>
2024-03-08 15:47:08 +0330

    Data fetching and error handling in typescript

---
+++ 5-sideEffects/src/util/http.ts
@@ -0,0 +1,27 @@
+/** @format */
+
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+export async function get(url: string) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Failed to fetch data.");
+  }
+  const data = (await response.json()) as unknown;
+  return data;
+}
+
+export async function getAxios(url: string) {
+  try {
+    const { data }: AxiosResponse = await axios.get(url);
+    return data;
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      console.log("Error message:", err.message);
+      return AxiosError;
+    } else {
+      console.log("Unexpected error:", err);
+      return "An unexpected error ocured";
+    }
+  }
+}
