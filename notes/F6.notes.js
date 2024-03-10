4d23e7281a487ebf4578bb65ad0ac918dd953d62
Sizzlesina <www.sins604@gmail.com>
2024-03-10 12:00:06 +0330

    Creating a custom dispatch function

--- notes/F6.notes.js
+++ notes/F6.notes.js
@@ -45,4 +45,18 @@ const initialState : SliceState = {
 @ How to use the redux store?:
 - We can wrap our application "App Component" into an Provider component that got imported from react/redux then it needs a store prop which the value of that is the store we create earlier
 
+
+@ How to dispatch a function into an handler function inside the component:
+- For type safety we may create a hooks.ts file and above the store file create our own custom type and then pass in the store.dispatch to this type
+++ Example:
+type AppDispatch = typeof store.dispatch
+
+- Now in the hooks.ts we create a function type and pass in the AppDispatch type to the return type of this funnction 
+++ Example:
+type DispatchFunction = () => AppDispatch;
+
+- Then we create a custom dispatch function and then pass in the "DispatchFunction" type to this variable
+++ Example:
+import {useDispatch} from 'react-redux'
+export const useSliceNameDispatch : DispatchFunction = useDispatch 
 */
